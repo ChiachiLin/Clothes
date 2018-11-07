44048471d9114b670d4bfd1cb5b60a94dafd5cdb
JackyLin <jacky21807@gmail.com>
2018-11-07 21:17:10 +0800

    add price

--- Clothes.java
+++ Clothes.java
@@ -3,14 +3,20 @@ package Clothes;
 public class Clothes{
 	private String size;
 	private String color;
+	private int price;
 	
 	public Clothes(){}
 	public Clothes(String size){
-		this.size = size;
+		this ();
+		setSize(size);
 	}
 	public Clothes(String size,String color){
 		this(size);
-		this.color = color;
+		setColor(color);
+	}
+	public Clothes(String size,String color,int price){
+		this(size,color);
+		setPrice(price);
 	}
 	public String getSize(){
 		return this.size;
@@ -24,5 +30,6 @@ public class Clothes{
 	public void setColor(String color){
 		this.color = color;
 	}
+	
 
 }
