b00fe33580161a939d0170125762b29c5e74b507
JackyLin <jacky21807@gmail.com>
2018-11-09 21:52:28 +0800

    add interface Waterproof.java,RainCoat.java

---
+++ src/Clothes/RainCoat.java
@@ -0,0 +1,30 @@
+package Clothes;
+
+public class RainCoat extends Clothes implements Waterproof {
+
+	public RainCoat() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public RainCoat(String size) {
+		super(size);
+		// TODO Auto-generated constructor stub
+	}
+
+	public RainCoat(String size, String color) {
+		super(size, color);
+		// TODO Auto-generated constructor stub
+	}
+
+	public RainCoat(String size, String color, int price) {
+		super(size, color, price);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void waterproof() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
